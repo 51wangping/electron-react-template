0cb657ad6793168168ed510911877732b3491462
heyuanjie <179708009@qq.com>
2021-12-20 13:36:53 +0800

    fix：eslint -jsx报错

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,14 @@
 module.exports = {
   extends: ['react-app'],
-  // plugins: ['prettier', 'simple-import-sort'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', '@typescript-eslint'],
   rules: {
+    "react/jsx-uses-react": 0,
+    "react/jsx-uses-vars": 0,
+    "react/react-in-jsx-scope": 0,
     // 'prettier/prettier': 'warn',
     // 'no-unused-vars': 'off',
     // eqeqeq: 'error',
-    // '@typescript-eslint/no-unused-vars': ['error'],
     // 'simple-import-sort/imports': 'error',
     // 'simple-import-sort/exports': 'error',
     // 'react-hooks/exhaustive-deps': 'warn'
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-import React from 'react';
 import './index.less';
 const app = () => {
   return <h1>helloworld</h1>;
